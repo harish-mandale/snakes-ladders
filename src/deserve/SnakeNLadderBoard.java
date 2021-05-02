719f033c0470062a2f1ce7414ceff57c88945e9c
harish <harish@salesbock.com>
2021-05-02 13:33:28 +0530

    Restructre for JUnit

--- src/deserve/SnakeNLadderBoard.java
+++ src/deserve/SnakeNLadderBoard.java
@@ -3,58 +3,71 @@ package deserve;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class SnakeNLadderBoard {
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	final static int WIN_POSITION = 100;
+	final static int TOTAL_TURNS = 10;
+	private final Map<Integer, Integer> snakes = new HashMap<>();
+	private final Map<Integer, Integer> ladders = new HashMap<>();
+	private int actualTurn = 0;
+	private int playerPosition = 0;
 
-	final static int WIN = 100;
-	final static int TURNS = 100;
-	public static void main(String args[])
-	{
-		Map<Integer, Integer> snakes = new HashMap<>();
-		snakes.put(14,7);
-		Map<Integer, Integer> ladders = new HashMap<>();
-		ladders.put(9,25);
-		ladders.put(37,88);
-		int player = 0;
-		int turn = 1;
-		while(turn <= TURNS)
-		{
-			int diceValue = rollDice();
-			player = player + diceValue;
-			player = snakeNLadderCheck(player, snakes);
-			player = snakeNLadderCheck(player, ladders);
-			if(isWinner(player))
-			{
-				System.out.println("Player won the Snakes-Ladders game:"+ player);
-				break;
+	public void addSnakes(final Map<Integer, Integer> snakes){
+		this.snakes.putAll(snakes);
+	}
+
+	public void addLadders(final Map<Integer, Integer> ladders){
+		this.ladders.putAll(ladders);
+	}
+	public boolean ifExceedingWinPosition(final int playerPosition, final int diceValue){
+		return playerPosition + diceValue > WIN_POSITION;
+	}
+
+	public int nextMove(final int diceValue) throws IllegalArgumentException{
+		if(diceValue == -1)
+		   throw new IllegalArgumentException();
+		if(actualTurn <= TOTAL_TURNS) {
+			if(ifExceedingWinPosition(playerPosition, diceValue)){
+				return playerPosition;
 			}
-			turn++;
+			updatePlayerPosition(diceValue);
+			playerPosition = snakeNLadderCheck();
+			if(isWinner(playerPosition)) {
+				LOGGER.info("Player won the Snakes-Ladders game:"+ playerPosition);
+				return playerPosition;
+			}
+			incrementActualTurn();
+		}else{
+			System.out.println("All turns exausted");
+			LOGGER.info("All turns exausted");
 		}
-		
+		return playerPosition;
+	}
+
+	public int snakeNLadderCheck(){
+		playerPosition = snakes.getOrDefault(playerPosition, playerPosition);
+		playerPosition = ladders.getOrDefault(playerPosition, playerPosition);
+		return playerPosition;
 	}
 	
-	public static int snakeNLadderCheck(int player, Map<Integer, Integer> map)
-	{
-		for(Map.Entry<Integer, Integer> item: map.entrySet())
-		{
-			if(player == item.getKey()) {
-				player = item.getValue();
-			    break;
-			}
-		}
-		return player;
+	public void incrementActualTurn() {
+		actualTurn++;
 	}
 	
-	public static int rollDice()
-	{
+	public void updatePlayerPosition(int value) {
+		playerPosition = playerPosition + value;
+	}
+	
+	public int rollEvenNumbersDice(){
 		Random rand = new Random();
 		int randomNum = rand.nextInt(7);
-		int num =  randomNum % 2 == 0 ? randomNum : rollDice();
+		int num =  randomNum % 2 == 0 ? randomNum : rollEvenNumbersDice();
 		return num;
 	}
 	
-	public static boolean isWinner(int player)
-	{
-		return player >= WIN; 
+	public boolean isWinner(int playerPosition) {
+		return playerPosition == WIN_POSITION;
 	}
 }
